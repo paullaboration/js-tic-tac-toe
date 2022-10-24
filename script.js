bee92f93d89edee92e58fb581ce77895d28ef581
paullaboration <juleskwoon@gmail.com>
2022-10-24 21:27:10 +0800

    add checkWin function

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const X_CLASS = 'x'
 const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2], 
+    [3, 4, 5], 
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6]
+]
 const cellElements = document.querySelectorAll('[data-cell]')
 const board = document.getElementById('board')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
 let circleTurn
 
 startGame()
-
 
 function startGame() {
     circleTurn = false
@@ -19,13 +28,24 @@ function handleClick(e) {
     const cell = e.target
     const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
     placeMark(cell, currentClass)
-
+    if (checkWin(currentClass)) {
+        endGame(false)
+    }
     // Check For Win
     // Check For Draw
     // Switch Turns
 
     swapTurns()
     setBoardHoverClass()
+}
+
+function endGame(draw) {
+    if (draw) {
+
+    } else {
+        winningMessageTextElement.innerText = `$(circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
 }
 
 function placeMark(cell, currentClass) {
@@ -46,3 +66,10 @@ function setBoardHoverClass() {
     }
 }
 
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every(index => {
+            return cellElements[index].classList.contains(currentClass)
+        })
+    })
+}
